a51f677761a8a264d796a406a2ad47b8ec45ec36
wzc-OntarioTechU <william.chamberlain@ontariotechu.net>
2024-10-02 23:55:37 -0400

    Should be working...

--- app/src/main/java/wzc/sofe4640u/mortgagecalculator/MainActivity.java
+++ app/src/main/java/wzc/sofe4640u/mortgagecalculator/MainActivity.java
@@ -1,12 +1,19 @@
 package wzc.sofe4640u.mortgagecalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FloatingActionButton calculateBtn = (FloatingActionButton) findViewById(R.id.calculateBtn);
+        calculateBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                double principal = Double.parseDouble(((EditText) findViewById(R.id.principalInput)).getText().toString());
+                double interest = Double.parseDouble(((EditText) findViewById(R.id.interestInput)).getText().toString());
+                interest /= 12;
+                int interestPeriod = Integer.parseInt(((EditText) findViewById(R.id.interestPeriodInput)).getText().toString());
+                interestPeriod *= 12;
+                int amortizationPeriod = Integer.parseInt(((EditText) findViewById(R.id.amortizationPeriodInput)).getText().toString());
+                amortizationPeriod *= 12;
+
+                double emi = principal * interest * (Math.pow(1 + interest, interestPeriod) / (Math.pow(1 + interest, interestPeriod) - 1));
+                double totalPaid = emi * amortizationPeriod;
+                double remaining = principal;
+                double interestAmount = 0, principalAmount = 0;
+                for (int i = 0; i < amortizationPeriod; i++) {
+                    interestAmount += principal * interest;
+                    principalAmount += emi - (principal * interest);
+                }
+                remaining -= principalAmount;
+
+                DecimalFormat formatter = new DecimalFormat("$###,###,###,##0.00");
+
+                Intent intent = new Intent(MainActivity.this, ResultsPage.class);
+                intent.putExtra("monthlyPayment", formatter.format(emi));
+                intent.putExtra("totalPaid", formatter.format(totalPaid));
+                intent.putExtra("principalPaid", formatter.format(principalAmount));
+                intent.putExtra("interestPaid", formatter.format(interestAmount));
+                intent.putExtra("remainingPrincipal", formatter.format(remaining));
+                startActivity(intent);
+            }
+        });
+
+        /* MaterialSwitch lumpSumSwitch = (MaterialSwitch) findViewById(R.id.lumpSumSwitch);
+        lumpSumSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                findViewById(R.id.additionalPaymentAmount).setVisibility(View.VISIBLE);
+                findViewById(R.id.additionalPaymentFrequency).setVisibility(View.VISIBLE);
+            } else {
+                findViewById(R.id.additionalPaymentAmount).setVisibility(View.INVISIBLE);
+                findViewById(R.id.additionalPaymentFrequency).setVisibility(View.INVISIBLE);
+            }
+        }); */
     }
+
 }
--- app/src/main/java/wzc/sofe4640u/mortgagecalculator/ResultsPage.java
+++ app/src/main/java/wzc/sofe4640u/mortgagecalculator/ResultsPage.java
@@ -1,12 +1,16 @@
 package wzc.sofe4640u.mortgagecalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class ResultsPage extends AppCompatActivity {
 
@@ -20,5 +24,17 @@ public class ResultsPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+        try {
+            double d;
+            ((EditText) findViewById(R.id.repeatPaymentAmount)).setText(intent.getStringExtra("monthlyPayment"));
+            ((EditText) findViewById(R.id.termPaymentTotalAmount)).setText(intent.getStringExtra("totalPaid"));
+            ((EditText) findViewById(R.id.interestTotalPaidAmount)).setText(intent.getStringExtra("interestPaid"));
+            ((EditText) findViewById(R.id.principalTotalPaidAmount)).setText(intent.getStringExtra("principalPaid"));
+            ((EditText) findViewById(R.id.finalBalanceAmount)).setText(intent.getStringExtra("remainingPrincipal"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
